2c4bda2b8551d53c672600006bd806beda453ddc
Max Kirchoff <max@lamasix.com>
2019-03-11 22:37:06 -0400

    initial commit

---
+++ app/assets/js/scripts.js
@@ -0,0 +1,28 @@
+/*!
+ * hehim
+ * hehim.guide
+ * https://github.com/LamaSix/hehim
+ * @author Max Kirchoff
+ * @version 1.0.0
+ * Copyright 2019. MIT licensed.
+ */
+(function ($, window, document, undefined) {
+
+  'use strict';
+
+  $(function () {
+
+    var hehim = function() {
+
+      return {
+        init: function() {
+
+        }
+      }
+    }
+
+    var app = hehim();
+    app.init();
+  });
+
+})(jQuery, window, document);
---
+++ src/js/scripts.js
@@ -0,0 +1,20 @@
+(function ($, window, document, undefined) {
+
+  'use strict';
+
+  $(function () {
+
+    var hehim = function() {
+
+      return {
+        init: function() {
+
+        }
+      }
+    }
+
+    var app = hehim();
+    app.init();
+  });
+
+})(jQuery, window, document);
