1ed48890cebe626478b0e93e626ccfb700b9c801
Maximillian Kirchoff <max@lamasix.com>
2020-07-30 14:22:04 -0500

    removed depricated deploy script

--- deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-printf '\n\n Syncing to S3 from local "public" folder.\n Excluding any dot files or folders.\n\n'
-aws s3 sync app/ s3://hehim.co --acl public-read --exclude=".*" --delete --size-only --region us-east-1
